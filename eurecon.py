314c5ac4c59e7ad4eec2a65c162a4941db0c48aa
fakeuser69498 <84857679+fakeuser69498@users.noreply.github.com>
2021-06-01 19:26:00 +0300

    Update eurecon.py

--- eurecon.py
+++ eurecon.py
@@ -12,23 +12,19 @@ from eurecon import Eurecon
 @click.option('--rmsd', '-r', help = 'Desired RMSD parameter')
 @click.option('--partition', '-p', help = 'Desired partition parameter; 0 - translation only, 1 - rotation only')
 @click.option('--axes_path', '-a', help = 'Path to the file, containing tessellation axes')
-@click.option('--relative_rmsd', '-rr', help = 'Calculating relative RMSD values based on maximum distance (0) from the center of the object or maximum bound values (1)', required = False)
-@click.option('--relative_rmsd_ratio', '-rrr', help = 'Proportional coefficient for the resulting realtive RMSD values', required = False)
 @click.option('--input_directory', '-i', help = 'Input directory for the base file')
 @click.option('--output_directory', '-o', help = 'Output directory for the generated conformations')
 @click.option('--stdout_mode', '-s', help = 'Enables/disables resulting file generation', required = False)
 @click.option('--weights_file', '-w', default = None, help = 'Path to the file, containing weights', required = False)
 @click.option('--debug', '-d', help = 'Enables resulting RMSD check for the generated conformations', required = False)
 
-def main(rmsd, partition, axes_path, relative_rmsd, relative_rmsd_ratio, input_directory, output_directory, stdout_mode, weights_file, debug):
+def main(rmsd, partition, axes_path, input_directory, output_directory, stdout_mode, weights_file, debug):
     eurecon = Eurecon(
         input_directory,
         axes_path,
         output_directory,
         float(rmsd),
         float(partition),
-        relative_rmsd,
-        relative_rmsd_ratio,
         bool(stdout_mode),
         weights_file,
         debug
